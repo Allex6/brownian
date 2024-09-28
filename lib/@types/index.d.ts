acb2a72cd88e37fe54edcb803850dd314666066e
Alex Souza <alex.webdevelloper@gmail.com>
2024-09-28 11:15:55 -0300

    :label: refactor: improve typings

--- lib/@types/index.d.ts
+++ lib/@types/index.d.ts
@@ -1,14 +1,38 @@
 declare module 'brownian' {
     /**
      * @description Generates a Brownian motion path
-     * @param start_x Starting x-coordinate
-     * @param start_y Starting y-coordinate
-     * @param steps Number of steps to take
-     * @returns Array of [x, y] positions
      */
-    export function brownian(
-        start_x: number,
-        start_y: number,
-        steps: number,
-    ): number[][];
+    export function brownian(options: BrownianOptions): number[][];
+
+    /**
+     * @description Options for the Brownian motion function
+     */
+    export interface BrownianOptions {
+        /**
+         * @description Starting x-coordinate
+         */
+        start_x: number;
+        /**
+         * @description Starting y-coordinate
+         */
+        start_y: number;
+        /**
+         * @description Number of steps to take
+         */
+        steps: number;
+        /**
+         * @description Time step between each step
+         */
+        time_step: number;
+    }
+
+    /**
+     * @description Output of the Brownian motion function
+     */
+    export interface BrownianOutput {
+        /**
+         * @description Array of [x, y] positions
+         */
+        path: number[][];
+    }
 }
