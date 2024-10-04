ed13bb9143d60979963a3326f9b6d5f2f6e87b98
Alex Souza <alex.webdevelloper@gmail.com>
2024-10-04 19:45:20 -0300

    :sparkles: feat: add particles factories

---
+++ lib/factories/particle.factory.ts
@@ -0,0 +1,27 @@
+import { Particle, ParticleCreation } from '../interfaces/particle.interface';
+
+/**
+ * @description Build a particle at its initial state
+ * @returns A particle
+ */
+export function createParticle(params: ParticleCreation): Particle {
+    return {
+        x: params.x,
+        y: params.y,
+        velocity: 0,
+        diffusion_coefficient: params.diffusion_coefficient,
+        total_distance_moved: 0,
+        distance_moved: 0,
+    };
+}
+
+/**
+ * @description Create multiple clones of a particle with the same properties
+ * @returns An array of particles
+ */
+export function createMultipleParticles(
+    params: ParticleCreation,
+    quantity: number,
+): Particle[] {
+    return Array.from({ length: quantity }, () => createParticle(params));
+}
